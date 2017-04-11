507bde0361ffb42c9ddab4534410c79a3c443abf
minhtridn2001 <minhtridn2001@gmail.com>
2017-04-11 13:28:43 +0700

    #28 Add APIs:  4. Forgot password.  5. Check reset password.  6. Set new password for user

--- web_app/app/Http/Controllers/UserController.php
+++ web_app/app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
     }
 
     /**
-     * API: Validate user.
+     * Validate user.
      *
      * @return \Illuminate\Http\Response
      */
@@ -95,6 +95,7 @@ class UserController extends Controller
     {
       $email = $request->email;
       $password = $request->currentPassword;
+      // TODO validate newPassword
       $newPassword = $request->newPassword;
       if (Auth::attempt(['email' => $email, 'password' => $password])) {
         $user = Auth::user();
@@ -113,6 +114,111 @@ class UserController extends Controller
                 'status_code' => 404
             ));
       }
+    }
+
+    /**
+     * Forgot password.
+     * @var email user's email.
+     * @return \Illuminate\Http\Response
+     */
+    public function forgotPassword(Request $request)
+    {
+      $email = $request->email;
+      $user = User::where('email',$email)->first();
+      if ($user == null) {
+        return response()->json(array(
+                  'error' => true,
+                  'content' => "This email does not exist!",
+                  'status_code' => 200
+              ));
+      }
+      // TODO generate random code and send SMS to user's phonenumber
+      $user->forgot_code = "12345";
+      date_default_timezone_set("Asia/Bangkok");
+      $date = date('Y-m-d G:i:s');
+      // $date->setTimezone(new DateTimeZone('Asia/Ho_Chi_Minh'));
+      // date_timezone_set($date, timezone_open('Asia/Bangkok'));
+      $user->forgot_code_created = $date;
+      $user->save();
+      return response()->json(array(
+                'error' => false,
+                'content' => "SMS has been sent!",
+                'status_code' => 200
+            ));
+    }
+
+    /**
+     * Check reset password code.
+     * @var email user's email
+     * @var confirmCode forgot password code.
+     * @return \Illuminate\Http\Response
+     */
+    public function checkResetCode(Request $request)
+    {
+      $email = $request->email;
+      $code = $request->confirmCode;
+      $user = User::where('email',$email)->first();
+      if ($user == null) {
+        return response()->json(array(
+                  'error' => true,
+                  'content' => "Wrong",
+                  'status_code' => 404
+              ));
+      }
+      $forgotCode = $user->forgot_code;
+      $forgotCodeTime = $user->forgot_code_created;
+      // TODO check experied time of the forgotCode
+      // date_default_timezone_set("Asia/Bangkok");
+      // $date = date('Y-m-d G:i:s');
+      if ($forgotCode == $code) {
+        $user->reset_password_token = "randomToken"; // TODO generate randomToken
+        $user->forgot_code = null;
+        $user->forgot_code_created = null;
+        $user->save();
+        return response()->json(array(
+                  'error' => false,
+                  'content' => array('reset_password_token' => $user->reset_password_token),
+                  'status_code' => 200
+              ));
+      } else {
+        return response()->json(array(
+                  'error' => true,
+                  'content' => "Wrong",
+                  'status_code' => 404
+              ));
+      }
+    }
+
+    /**
+     * Set new password for user.
+     * @var email user's email
+     * @var newPassword
+     * @var resetPasswordToken
+     * @return \Illuminate\Http\Response
+     */
+    public function setNewPassword(Request $request)
+    {
+      $email = $request->email;
+      $newPassword = $request->newPassword;
+      $resetPasswordToken = $request->resetPasswordToken;
+      $user = User::where('email',$email)->first();
+      // check if user is exist or reset password token is correct
+      if ($user == null || $user->reset_password_token != $resetPasswordToken) {
+        return response()->json(array(
+                  'error' => true,
+                  'content' => "Failed",
+                  'status_code' => 404
+              ));
+      }
+      $user->reset_password_token = null;
+      // TODO validate newPassword
+      $user->password = bcrypt($newPassword);
+      $user->save();
+      return response()->json(array(
+                'error' => false,
+                'content' => "Succeeded!",
+                'status_code' => 200
+            ));
     }
 
 
