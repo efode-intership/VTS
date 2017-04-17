d7b3c000de5f53dc423e43e06479f9a457099b2b
tuanhwing <tuanhwing2014@gmail.com>
2017-04-17 09:11:07 +0700

    #29

--- mobile_app/app/src/main/java/vn/efode/vts/MainActivity.java
+++ mobile_app/app/src/main/java/vn/efode/vts/MainActivity.java
@@ -1,14 +1,21 @@
 package vn.efode.vts;
 
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -23,23 +30,42 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
 
-import vn.efode.vts.service.ReadTask;
-import vn.efode.vts.service.TrackGPS;
+import java.util.ArrayList;
+
+import vn.efode.vts.utils.ReadTask;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
-    public static GoogleMap map = null;
-    private TrackGPS gps;
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+
+
+    public static GoogleMap mGoogleMap = null;
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+
+    LatLng latLng;
+    Marker currLocationMarker;
+
+    boolean temp = true;//Just zoom 1 time
+
+    PendingResult<LocationSettingsResult> result;
+    final private static int REQ_PERMISSION = 20;
     private static int REQUEST_LOCATION = 10;
     private static String API_KEY_DIRECTION = "AIzaSyAJCQ6Wf-aQbUbF5wLRMs4XtgCS-vph6IE";
     private static String API_KEY_MATRIX =  "AIzaSyCGXiVPlm9M72lupfolIXkxzSTPNIvRr8g";
@@ -198,40 +224,72 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-        gps = new TrackGPS(MainActivity.this);
+        mGoogleMap = googleMap;
 
-        if(!gps.canGetLocation()) {
+        makeMaker(new LatLng(10.8719808, 106.790409), "Nong Lam University");
 
-            gps.showSettingsAlert();
-
+        if(checkLocationPermission()){
+            mGoogleMap.setMyLocationEnabled(true);
+            buildGoogleApiClient();
         }
-        longitude = gps.getLongitude();
-        latitude = gps .getLatitude();
 
-        try {
-            LatLng currentMaker = new LatLng(latitude,longitude);
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentMaker, 15));
 
-            map.addMarker(new MarkerOptions().title("Current Location").position(currentMaker));
 
-            map.addPolyline(new PolylineOptions()
-                    .add( currentMaker, new LatLng(10.882323, 106.782625))
-                    .width(5)
-                    .color(Color.RED));
-        } catch (Exception e){
-            Log.d("MAPEXCEPTION", e.getMessage());
+
+
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQ_PERMISSION);
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQ_PERMISSION);
+            }
+            return false;
+        } else {
+            return true;
         }
-        Toast.makeText(getApplicationContext(),"Longitude:"+Double.toString(longitude)+"\nLatitude:"+Double.toString(latitude),Toast.LENGTH_SHORT).show();
+    }
 
 
-        //path of two point
-        String url = getMapsApiDirectionsUrl(new LatLng(latitude,longitude),  new LatLng(10.882323, 106.782625));
+    protected synchronized void buildGoogleApiClient() {
+        Toast.makeText(this, "buildGoogleApiClient", Toast.LENGTH_SHORT).show();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    /**
+     * Draw road between 2 location in google map
+     */
+    private void drawroadBetween2Location(LatLng latLng1, LatLng latLng2) {
+        ArrayList<LatLng> temp = new ArrayList<LatLng>();
+        temp.add(new LatLng(10.8719808, 106.790409));
+        String url = getMapsApiDirectionsUrl(latLng1, latLng2, temp);
 
         Log.d("onMapClick", url.toString());
         ReadTask downloadTask = new ReadTask();
         // Start downloading json data from Google Directions API
         downloadTask.execute(url);
+        Log.d("Log12/4", "12");
 
         //To calculate distance between points
 //        float[] results = new float[1];
@@ -239,7 +297,18 @@ public class MainActivity extends AppCompatActivity
 //                10.882323, 106.782625,
 //                results);
 //        Log.d("onMapClick",String.valueOf(results));
+    }
 
+    /**
+     * Make a Maker in google map
+     * @param location location maker
+     * @param title title maker
+     */
+    private void makeMaker(LatLng location, String title) {
+        Log.d("Log12/4", "9");
+        LatLng maker = new LatLng(location.latitude, location.longitude);
+        mGoogleMap.addMarker(new MarkerOptions().title(title).position(maker));
+        Log.d("Log12/4", "10");
     }
 
     /**
@@ -248,29 +317,41 @@ public class MainActivity extends AppCompatActivity
      * @param dest destination point location
      * @return
      */
-    private String getMapsApiDirectionsUrl(LatLng origin, LatLng dest) {
+    private String getMapsApiDirectionsUrl(LatLng origin, LatLng dest, ArrayList<LatLng> waypoints) {
         // Origin of route
-        String str_origin = "origin="+origin.latitude+","+origin.longitude;
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
 
         // Destination of route
-        String str_dest = "destination="+dest.latitude+","+dest.longitude;
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
 
 
         // Sensor enabled
         String sensor = "sensor=false";
 
+        //Waypoints
+        String str_waypoints = "waypoints=";
+        boolean firts = false;
+        for (LatLng latlng : waypoints) {
+            if (!firts) {
+                str_waypoints += "via:" + latlng.latitude + "," + latlng.longitude;
+                firts = true;
+            } else {
+                str_waypoints += "|via:" + latlng.latitude + "," + latlng.longitude;
+            }
+        }
+
         //key
-        String keyDirection = "key="+API_KEY_DIRECTION;
+        String keyDirection = "key=" + API_KEY_DIRECTION;
 
         // Building the parameters to the web service
 //        String parameters = str_origin+"&"+str_dest+"&"+sensor;
-        String parameters = str_origin+"&"+str_dest+"&"+keyDirection;
+        String parameters = str_origin + "&" + str_dest + "&" + str_waypoints + "&" + keyDirection;
 
         // Output format
         String output = "json";
 
         // Building the url to the web service
-        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters;
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
 
         return url;
 
@@ -278,42 +359,148 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_LOCATION) {
-            if(grantResults.length == 1
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // We can now safely use the API we requested access to
-                Log.d("LOCATIONNETWORK","result");
+        Log.d("Permission", "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch ( requestCode ) {
+            case REQ_PERMISSION: {
+                if ( grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+                    // permission was granted, yay!
+                    if (ActivityCompat.checkSelfPermission(this,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        mGoogleMap.setMyLocationEnabled(true);
+                        buildGoogleApiClient();
 
-            } else {
-                // Permission was denied or request was cancelled
+                    }
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
             }
         }
     }
-
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        gps.stopUsingGPS();
+        //Unregister receiver on destroy
+        if (gpsReceiver != null)
+            unregisterReceiver(gpsReceiver);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        registerReceiver(gpsReceiver, new IntentFilter("android.location.PROVIDERS_CHANGED"));//Register broadcast r
+    }
 
+
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(this, "onConnected", Toast.LENGTH_SHORT).show();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.INTERNET
+                }, 10);
+            }
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        android.Manifest.permission.INTERNET
+                }, 10);
+            }
+        }
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        Log.d("onConnected",String.valueOf(mLastLocation));
+        if (mLastLocation != null) {
+            //place marker at current position
+            //mGoogleMap.clear();
+//            latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+//            MarkerOptions markerOptions = new MarkerOptions();
+//            markerOptions.position(latLng);
+//            markerOptions.title("Current Position");
+//            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+////            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+//            currLocationMarker = mGoogleMap.addMarker(markerOptions);
+        }
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(50000); //5 seconds
+        mLocationRequest.setFastestInterval(30000); //30 seconds
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        //mLocationRequest.setSmallestDisplacement(0.1F); //1/10 meter
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+
+
+    }
+
+    private BroadcastReceiver gpsReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+            if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                //Do your stuff on GPS status change
+                Toast.makeText(MainActivity.this,"GPS enable!",Toast.LENGTH_LONG).show();
+            }
+            else  Toast.makeText(MainActivity.this,"GPS disable!",Toast.LENGTH_LONG).show();
+        }
+    };
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this,"onConnectionSuspended",Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == 1) {
-            switch (requestCode) {
-                case 1:
-
-            }
-        }
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(this,"onConnectionFailed",Toast.LENGTH_SHORT).show();
     }
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        //place marker at current position
+        //mGoogleMap.clear();
+        if (currLocationMarker != null) {
+            currLocationMarker.remove();
+        }
+        latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        if(temp){
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+            drawroadBetween2Location(latLng,new LatLng(10.8719808, 106.790409));
+            temp = false;
+        }
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(latLng);
+//        markerOptions.title("Current Position");
+//        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+//        currLocationMarker = mGoogleMap.addMarker(markerOptions);
+//
+        Toast.makeText(this,"Location Changed",Toast.LENGTH_SHORT).show();
+//
+//        //zoom to current position:
+//        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,11));
+
+        //If you only need one location, unregister the listener
+        //LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
 
     public void showWarning(){
         // custom dialog
